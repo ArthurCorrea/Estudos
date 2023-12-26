cdc61d35ccaec60f87c7df69994d0cb78232f293
arthurcorrea <arthurcorrea321321@gmail.com>
2023-12-26 20:35:11 -0300

    att project-costs

--- ReactJS/costs/src/App.jsx
+++ ReactJS/costs/src/App.jsx
@@ -1,10 +1,10 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Home from './components/pages/Home';
-//import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
@@ -22,6 +22,7 @@ function App() {
             <Route exact path='/projects' element={ <Projects/> }/>
             <Route exact path='/company' element={ <Company/> }/>
             <Route exact path='/contact' element={ <Contact/> }/>
+            <Route exact path='/newproject' element={ <NewProject/> }/>
         </Routes>
       </Container>
 
