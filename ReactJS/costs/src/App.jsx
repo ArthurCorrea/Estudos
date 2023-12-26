2ea4503aa739c64d8221623d3bf6195bed71354c
arthurcorrea <arthurcorrea321321@gmail.com>
2023-12-26 09:16:19 -0300

    att react

--- ReactJS/costs/src/App.jsx
+++ ReactJS/costs/src/App.jsx
@@ -1,33 +1,32 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Home from './components/pages/Home';
-import NewProject from './components/pages/NewProject';
+//import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
       
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-        <Link to="/company">Sobre nós</Link>
-        <Link to="/contact">Contato</Link>
-      </div>
+      <Navbar />
 
       <Container customClass="min-height">
         <Routes>
             <Route exact path='/' element={ <Home/> }/>
-            <Route exact path='/newproject' element={ <NewProject/> }/>
+            <Route exact path='/projects' element={ <Projects/> }/>
             <Route exact path='/company' element={ <Company/> }/>
             <Route exact path='/contact' element={ <Contact/> }/>
         </Routes>
       </Container>
 
-      <p>Footer</p>
+      <Footer />
+      
     </Router>
   );
 }
